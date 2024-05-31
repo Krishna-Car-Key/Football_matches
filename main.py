848967900b97e61539a55bdf614229aa20d61b1f
krishna <krishnakarki1996@gmail.com>
2024-05-31 22:32:44 +0545

    got datas and render them on webpage

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import streamlit as st
+from backend import get_data
 
+st.title("football Matches Results")
+matches, results, dates = get_data()
 
+length = len(matches)
+count = 0
 
+while count < length:
+    st.write(dates[count])
+    st.write(+ matches[count])
+    st.info(results[count])
+    count += 1
